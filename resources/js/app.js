ef7b0c64e56398e3068954aa31b02ba4b63351d6
Sayyed Azhar <asayyed910@gmail.com>
2021-10-12 00:53:38 +0530

    Task completed according to requirement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+window.Vue = Vue;
+
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import TransactionCategories from './components/TransactionCategories'
+
+Vue.use(VueAxios, axios);
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    TransactionCategories
+  }
+})
